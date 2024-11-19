26cf6c9aa7742e7eb2fae1e28fc39dd3131ef111
Lydiao8 <offorlydia@gmail.com>
2024-11-19 15:46:47 +0000

    Added new endpoint to start test.

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+app = FastAPI()
+
+
+@app.get("/start-test")
+def handle_request_from_external_clients():
+    return ["Hello world"]
+
+@app.get("/start-test/{user_name}")
+def read_item(user_name):
+    return "Hello " + user_name
